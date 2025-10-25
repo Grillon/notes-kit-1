3b04e0e62e228e5d21a4fb108978d0a8e3434c5e
Grillon <thierry.grillon@gmail.com>
2025-10-26 00:43:45 +0200

    v0.3.1: oublie d'un fichier dans commit

--- app/types.ts
+++ app/types.ts
@@ -1,9 +1,15 @@
-export type NoteID = string;
-
 export interface Note {
-  id: NoteID;
+  id: string;
   title: string;
   content: string;
   createdAt: string;
   updatedAt: string;
+}
+
+export interface ImageData {
+  id?: number;
+  noteId: string;
+  name: string;
+  data: Blob;
+  createdAt: string;
 }
